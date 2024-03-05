66a8e7785fae1dcda5dbaff82bb9aa4ce34c71bf
yixing <leiyixing@gmail.com>
2024-03-05 15:12:08 +0000

    M162

--- src/M162FindPeakElement.java
+++ src/M162FindPeakElement.java
@@ -1,21 +1,19 @@
 public class M162FindPeakElement {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 1) return 0; // single element
-        int n = nums.length;
-        // check if 0th/n-1th index is the peak element
-        if (nums[0] > nums[1]) return 0;
-        if (nums[n - 1] > nums[n - 2]) return n - 1;
+        return binarySearch(0, nums.length - 1, nums);
+    }
 
-        // search in the remaining array
-        int start = 1;
-        int end = n - 2;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
-            else if (nums[mid] < nums[mid - 1]) end = mid - 1;
-            else if (nums[mid] < nums[mid + 1]) start = mid + 1;
+    public int binarySearch(int start, int end, int[] nums) {
+        if (start == end) return start;
+        else if (start + 1 == end) return nums[start] > nums[end] ? start : end;
+        else {
+            while (start < end) {
+                int mid = (start + end) / 2;
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+                else if (nums[mid] < nums[mid - 1]) return binarySearch(start, mid, nums);
+                else if (nums[mid] < nums[mid + 1]) return binarySearch(mid, end, nums);
+            }
         }
-        return -1; // dummy return statement
+        return -1;
     }
 }
