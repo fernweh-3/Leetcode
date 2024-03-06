5c3504dbaaf47c1fbcee51e5c1fa8b6512b9de80
yixing <leiyixing@gmail.com>
2024-03-06 16:12:08 +0000

    M189

---
+++ src/M189RotateArray.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class M189RotateArray {
+
+
+    public void rotate(int[] nums, int k) {
+        if (k > nums.length) k = k % nums.length;
+        int[] right = Arrays.copyOf(nums, nums.length - k);
+        int[] left = Arrays.copyOfRange(nums, nums.length - k, nums.length);
+
+        for (int i = 0; i < k; i++) {
+            nums[i] = left[i];
+        }
+        for (int i = k; i < nums.length; i++) {
+            nums[i] = right[i - k];
+        }
+
+    }
+}
