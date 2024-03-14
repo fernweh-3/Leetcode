9ecb72f447f7a2dea8f4505e4e39d9eb0c391163
yixing <leiyixing@gmail.com>
2024-03-14 12:49:43 +0000

    M875

--- src/M875KokoEatingBananas.java
+++ src/M875KokoEatingBananas.java
@@ -3,7 +3,28 @@ import java.util.Arrays;
 
 public class M875KokoEatingBananas {
     public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        long left = 1;
+        long right = piles[piles.length - 1];
 
-        return 1;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (canEatInTime(piles, mid, h))
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return (int) left;
+    }
+
+    public boolean canEatInTime(int piles[], long k, long h) {
+        long hours = 0;
+        for (int pile : piles) {
+            long div = pile / k;
+            hours += div;
+            if (pile % k != 0)
+                hours++;
+        }
+        return hours <= h;
     }
 }
