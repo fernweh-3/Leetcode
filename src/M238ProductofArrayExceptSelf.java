722ea5c25faef91d74b517b6bbde3db70ac3a96a
yixing <leiyixing@gmail.com>
2024-03-14 12:21:22 +0000

    M238

---
+++ src/M238ProductofArrayExceptSelf.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class M238ProductofArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int res[] = new int[nums.length];
+        int prefix = 1;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return res;
+    }
+}
