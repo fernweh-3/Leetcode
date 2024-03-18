2403967e38cd1d48c842ff4581461d4d7cd3babd
yixing <leiyixing@gmail.com>
2024-03-18 16:43:08 +0000

    E374

--- src/E374GuessNumberHigherLower.java
+++ src/E374GuessNumberHigherLower.java
@@ -1,13 +1,14 @@
-/**
- * Forward declaration of guess API.
- *
- * @param num your guess
- * @return -1 if num is higher than the picked number
- * 1 if num is lower than the picked number
- * otherwise return 0
- * int guess(int num);
- */
+
 public class E374GuessNumberHigherLower {
+    /**
+     * Forward declaration of guess API.
+     *
+     * @param num your guess
+     * @return -1 if num is higher than the picked number
+     * 1 if num is lower than the picked number
+     * otherwise return 0
+     * int guess(int num);
+     */
     int guess(int num) {
         return 0;
     }
