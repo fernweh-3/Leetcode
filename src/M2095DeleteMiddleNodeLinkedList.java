f8572a91c59c628c4306d01d90e1a34d64627b04
yixing <leiyixing@gmail.com>
2024-03-18 14:21:41 +0000

    E1732

---
+++ src/M2095DeleteMiddleNodeLinkedList.java
@@ -0,0 +1,13 @@
+public class M2095DeleteMiddleNodeLinkedList {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+
+    }
+}
