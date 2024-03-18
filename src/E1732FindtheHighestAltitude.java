90b3e019a7ee05ee1b2e2c59f72197cee7b31b85
yixing <leiyixing@gmail.com>
2024-03-18 13:53:17 +0000

    E1732

---
+++ src/E1732FindtheHighestAltitude.java
@@ -0,0 +1,10 @@
+public class E1732FindtheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int max = Math.max(0, gain[0]);
+        for (int i = 1; i < gain.length; i++) {
+            gain[i] = gain[i] + gain[i - 1];
+            max = Math.max(max, gain[i]);
+        }
+        return max;
+    }
+}
