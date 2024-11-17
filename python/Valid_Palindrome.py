4091c87095826e101a85780123a85bb3bf314a49
yixing <leiyixing@gmail.com>
2024-11-17 15:47:18 +0000

    isPalindrome

---
+++ python/Valid_Palindrome.py
@@ -0,0 +1,29 @@
+def isPalindrome(self, s: str) -> bool:
+    l, r = 0, len(s) - 1
+    while l < r:
+        while l < r and not self.alphaNum(s[l]):
+            l += 1
+        while r > l and not self.alphaNum(s[r]):
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l, r = l + 1, r - 1
+    return True
+
+
+ def alphaNum(self, c):
+    return (ord('A') <= ord(c) <= ord('Z') or
+            ord('a') <= ord(c) <= ord('z') or
+            ord('0') <= ord(c) <= ord('9'))
+
+# reverse the string and compare
+# def isPalindrome(self, s: str) -> bool:
+#         newStr = ''
+#         for c in s:
+#             if c.isalnum():
+#                 newStr += c.lower()
+#         return newStr == newStr[::-1]
+
+#isalnum() is a built-in method used in string manipulations in Python.
+# The isalnum() method returns “True” if all characters in the string are alphanumeric (either alphabets or numbers).
+# If not, it returns “False”.
