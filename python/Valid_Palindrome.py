9b7c06ec3130f3580fd3bbad08f4829ff3f69499
yixing <leiyixing@gmail.com>
2024-11-17 15:53:26 +0000

    isPalindrome

--- python/Valid_Palindrome.py
+++ python/Valid_Palindrome.py
@@ -11,10 +11,24 @@ def isPalindrome(self, s: str) -> bool:
         l, r = l + 1, r - 1
     return True
 
+# a helper function to check if a character is alphanumeric
 def alphaNum(self, c: str) -> bool:
     return (ord('A') <= ord(c) <= ord('Z') or
             ord('a') <= ord(c) <= ord('z') or
             ord('0') <= ord(c) <= ord('9'))
+
+# use isalnum() to check if a character is alphanumeric
+def isPalindrome(self, s: str) -> bool:
+    left, right = 0, len(s) - 1
+    while left<right:
+        while left<right and not s[left].isalnum():
+            left += 1
+        while left<right and not s[right].isalnum():
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False
+        left, right = left + 1, right - 1
+    return True
 
 # reverse the string and compare
 # def isPalindrome(self, s: str) -> bool:
