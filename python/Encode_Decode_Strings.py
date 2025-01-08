79db7edef323cdee91751f6c67cb5b24c95b1d14
yixing <leiyixing@gmail.com>
2025-01-08 21:20:37 +0000

    Encode and Decode Strings

---
+++ python/Encode_Decode_Strings.py
@@ -0,0 +1,15 @@
+def encode(self, strs: List[str]) -> str:
+    res =""
+    for s in strs:
+        res += f"{len(s):03}" + s
+    return res
+
+def decode(self, s: str) -> List[str]:
+    i = 0
+    res=[]
+    while i<len(s):
+        length = int(s[i:i+3])
+        i+=3
+        res.append(s[i:i + length])
+        i+=length
+    return res
