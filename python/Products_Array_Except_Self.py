4ffb890e21f62fa437ee9d15b1fce30f61515820
yixing <leiyixing@gmail.com>
2025-01-08 21:50:38 +0000

    productExceptSelf

--- python/Products_Array_Except_Self.py
+++ python/Products_Array_Except_Self.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def productExceptSelf(self, nums: List[int]) -> List[int]:
-    res = [1]*len(nums)
+    res = [1]*(len(nums))
     for i in range(len(nums)-1):
         res[i+1] = res[i]*nums[i]
     postfix = 1
