6639a38ed03c1336769445aff5c1e8719e015cde
yixing <leiyixing@gmail.com>
2025-01-19 22:14:37 +0000

    dailyTemperatures

--- python/Daily_Temperatures.py
+++ python/Daily_Temperatures.py
@@ -18,3 +18,13 @@ def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
                 count+=1
     res.append(0)
     return res
+
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    res = [0]*len(temperatures)
+    stack = []
+    for idx, temp in enumerate(temperatures):
+        while stack and temp>stack[-1][0]:
+            t, i = stack.pop()
+            res[i] = idx - i
+        stack.append((temp, idx))
+    return res
