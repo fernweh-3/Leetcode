70d45cb829beb936db2a3e5b09906d652f8fa978
yixing <leiyixing@gmail.com>
2025-01-20 23:12:08 +0000

    search

--- python/Search_Rotated_Sorted_Array.py
+++ python/Search_Rotated_Sorted_Array.py
@@ -2,20 +2,22 @@ from typing import List
 
 
 def search(self, nums: List[int], target: int) -> int:
-    l , r = 0, len(nums)-1
-    while l<=r:
-        mid = (l+r)//2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > nums[r]:
-            if target > nums[mid] or target < nums[r]:
-                l = mid + 1
-            else:
-                r = mid - 1
-        else:
-            if target < nums[mid] or target > nums[l]:
-                r = mid - 1
-            else:
-                l = mid + 1
+    l, r = 0, len(nums) - 1
 
+    while l <= r:
+        mid = (l + r) // 2
+        if target == nums[mid]:
+            return mid
+
+        if nums[l] <= nums[mid]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
     return -1
