950353d9fd58b78fd7b3a850d7359fa4fd541231
yixing <leiyixing@gmail.com>
2025-01-20 22:57:42 +0000

    search

---
+++ python/Search_Rotated_Sorted_Array.py
@@ -0,0 +1,18 @@
+def search(self, nums: List[int], target: int) -> int:
+    l , r = 0, len(nums)-1
+    while l<=r:
+        mid = (l+r)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > nums[r]:
+            if target > nums[mid] or target < nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        else:
+            if target < nums[mid] or target > nums[l]:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+    return -1
