9157f9f1e5d8fb6fc280822d249ea53dcfa87381
yixing <leiyixing@gmail.com>
2025-01-26 20:13:05 +0000

    mergeTwoLists

--- python/Merge_Two_Sorted_Linked_Lists.py
+++ python/Merge_Two_Sorted_Linked_Lists.py
@@ -1,3 +1,11 @@
+from typing import Optional
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
 def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
     if list1 is None:
         return list2
@@ -9,3 +17,20 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
     else:
         list2.next = self.mergeTwoLists(list1, list2.next)
         return list2
+
+
+def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+    dummy = node = ListNode()
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            node.next = list1
+            list1 = list1.next
+        else:
+            node.next = list2
+            list2 = list2.next
+        node = node.next
+
+    node.next = list1 or list2
+
+    return dummy.next
