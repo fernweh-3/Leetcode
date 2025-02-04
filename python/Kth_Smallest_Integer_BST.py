ff09de88a4f5bf78c0904f8d1a3841a11c51f16e
yixing <leiyixing@gmail.com>
2025-02-04 23:58:46 +0000

    Kth Smallest Integer in BST

--- python/Kth_Smallest_Integer_BST.py
+++ python/Kth_Smallest_Integer_BST.py
@@ -22,3 +22,17 @@ class Solution:
 
         dfs(root)
         return arr[k - 1]
+
+    def kthSmallestInorder(self, root: Optional[TreeNode], k: int) -> int:
+            arr = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.left)
+            arr.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+        return arr[k - 1]
