8ad38894273cf2cb766f88962f31b18850045e34
yixing <leiyixing@gmail.com>
2025-02-08 23:52:27 +0000

    Construct Binary Tree from Preorder and Inorder Traversal

--- python/Construct_Binary_Tree.py
+++ python/Construct_Binary_Tree.py
@@ -15,3 +15,21 @@ def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNod
         root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
         return root
+
+def buildTreeDFS(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+                indices = {val: idx for idx, val in enumerate(inorder)}
+
+                self.pre_idx = 0
+                def dfs(l, r):
+                        if l > r:
+                                return None
+
+                        root_val = preorder[self.pre_idx]
+                        self.pre_idx += 1
+                        root = TreeNode(root_val)
+                        mid = indices[root_val]
+                        root.left = dfs(l, mid - 1)
+                        root.right = dfs(mid + 1, r)
+                        return root
+
+                return dfs(0, len(inorder) - 1)
