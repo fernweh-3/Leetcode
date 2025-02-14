6dc7d70654077d1bc3ac739a64f730bad8672826
yixing <leiyixing@gmail.com>
2025-02-14 13:41:18 +0000

    48. Rotate Image

---
+++ java/M48RotateImage.java
@@ -0,0 +1,20 @@
+public class M48RotateImage {
+    public void rotate(int[][] matrix) {
+        // transpose
+        for(int i= 0; i < matrix.length; i++){
+            for (int j = i; j< matrix[i].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // reverse
+        for(int i= 0; i < matrix.length; i++){
+            for (int j = 0; j< matrix[i].length/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - j - 1];
+                matrix[i][matrix[i].length - j - 1] = temp;
+            }
+        }
+    }
+}
