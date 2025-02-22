9ea9f20b8a4890e6e4a74d402d03510ff1aa2a58
yixing <leiyixing@gmail.com>
2025-02-22 23:57:45 +0000

    Remove Node From End of Linked List

---
+++ java/RemoveNodeFromEndLinkedList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveNodeFromEndLinkedList {
+
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            nodes.add(cur);
+            cur = cur.next;
+        }
+
+        int removeIndex = nodes.size() - n;
+        if (removeIndex == 0) {
+            return head.next;
+        }
+
+        nodes.get(removeIndex - 1).next = nodes.get(removeIndex).next;
+        return head;
+
+    }
+}
