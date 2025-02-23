7fb6688c445b6de55815a3aff43631ec93672135
yixing <leiyixing@gmail.com>
2025-02-23 23:56:52 +0000

    Remove Node From End of Linked List

--- java/RemoveNodeFromEndLinkedList.java
+++ java/RemoveNodeFromEndLinkedList.java
@@ -33,4 +33,28 @@ public class RemoveNodeFromEndLinkedList {
         return head;
 
     }
+
+    public ListNode removeNthFromEndIteration(ListNode head, int n) {
+        int N = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            N++;
+            cur = cur.next;
+        }
+
+        int removeIndex = N - n;
+        if (removeIndex == 0) {
+            return head.next;
+        }
+
+        cur = head;
+        for (int i = 0; i < N - 1; i++) {
+            if ((i + 1) == removeIndex) {
+                cur.next = cur.next.next;
+                break;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
 }
