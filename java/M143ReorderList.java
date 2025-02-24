8921d5b46c774e3565291285046defe1d79e2c01
yixing <leiyixing@gmail.com>
2025-02-24 23:24:05 +0000

    Reorder Linked List

---
+++ java/M143ReorderList.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class M143ReorderList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            nodes.add(cur);
+            cur = cur.next;
+        }
+
+        int i = 0, j = nodes.size() - 1;
+        while (i < j) {
+            nodes.get(i).next = nodes.get(j);
+            i++;
+            if (i >= j) {
+                break;
+            }
+            nodes.get(j).next = nodes.get(i);
+            j--;
+        }
+
+        nodes.get(i).next = null;
+
+    }
+
+
+}
